c754a3910ae081d926963d747facac21f9052eee
soapity <mrows9356@gmail.com>
2024-09-08 16:10:15 -0400

    got rid of the links

--- src/lib/General/Footer.svelte
+++ src/lib/General/Footer.svelte
@@ -10,12 +10,6 @@
                 <a target = "_blank" href = "https://www.instagram.com/pigeonbots/"><i class = "fa-brands fa-instagram"></i></a>
                 <a target = "_blank" href = "https://twitter.com/FTC23975"><i class = "fa-brands fa-twitter"></i></a>
                 <a target = "_blank" href = "mailto:pigeonbotsftc@gmail.com"><i class = "fa-solid fa-envelope"></i></a>
-                <br><br>
-                <a target='_blank' href = 'https://docs.google.com/spreadsheets/d/1Dd0fLdFYP36DSvNPj6YCv0iOk0ZdvwZso3GQvOSVOJw/edit?usp=sharing'>
-                    <i class = 'fs-6 text-secondary'>Edit Text</i></a>
-                <a target='_blank' href = 'https://console.firebase.google.com/u/0/project/nope-85379/storage/nope-85379.appspot.com/files'>
-                    <i class = 'fs-6 text-secondary'>Edit Images</i></a>
-
             </div>
         </div>
 
