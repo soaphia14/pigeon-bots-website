7731e43171eba02a3cd128860e5824903ef934bf
ChristoDiamond <94727960+ChristoDiamond@users.noreply.github.com>
2026-08-30 16:22:13 -0400

    new event page

--- src/lib/Pages/Achievements.svelte
+++ src/lib/Pages/Achievements.svelte
@@ -13,20 +13,24 @@
         done = true;
     });
 </script>
-<h1 class = {title_class_def}>Achievements</h1>
 
-<div data-robolyst-embed="https://robolyst.org/embed?c=eyJ2IjoxLCJzdWJqZWN0Ijp7ImtpbmQiOiJ0ZWFtIiwiaWQiOiIyMzk3NSIsInNlYXNvbiI6bnVsbH0sImJsb2NrcyI6W3sidCI6InRlYW0tY2FyZCIsInMiOnsic2hvd0xvY2F0aW9uIjp0cnVlLCJzaG93Um9va2llIjp0cnVlLCJzaG93UmVjb3JkIjp0cnVlLCJzaG93RXZlbnRzIjp0cnVlLCJzaG93QXdhcmRzIjp0cnVlfX0seyJ0IjoidGVhbS11cGNvbWluZyIsInMiOnsiY291bnQiOjMsInNob3dDb3VudGRvd24iOnRydWV9fSx7InQiOiJ0ZWFtLWV2ZW50cyIsInMiOnsiY291bnQiOjMsInNob3dSZWNvcmQiOnRydWUsInNob3dSYW5rIjp0cnVlLCJzaG93QXdhcmRzIjp0cnVlLCJzaG93RGF0ZXMiOnRydWV9fSx7InQiOiJ0ZWFtLW1ldHJpY3MiLCJzIjp7Im1ldHJpY3MiOlsib3ByIiwib3ByYyIsInN0cmVuZ3RoIl0sImJhc2lzIjoiYmVzdCIsImxheW91dCI6ImdyaWQifX0seyJ0IjoidGVhbS1hd2FyZHMiLCJzIjp7ImxpbWl0Ijo2MCwiZ3JvdXBCeVNlYXNvbiI6dHJ1ZSwic3R5bGUiOiJ3YWxsIn19XSwibGF5b3V0Ijp7InciOjAsImgiOjE4NzksImNvbHMiOjEsImdhcCI6MTIsInBhZCI6MTYsInJhZGl1cyI6MTQsImRlbnNpdHkiOiJjb21mb3J0YWJsZSIsImRpciI6ImNvbHVtbiJ9LCJ0aGVtZSI6eyJwcmVzZXQiOiJkYXJrIiwiYmciOiIwMjA0MTAiLCJzdXJmYWNlIjoiMTYxNjE2IiwiYm9yZGVyIjoiMjcyNjI1IiwidGV4dCI6ImZkZmRmZCIsIm11dGVkIjoiODg4ODg4IiwiYWNjZW50IjoiOGM1MmZmIiwicG9zIjoiNGFkZTgwIiwibmVnIjoiZjg3MTcxIiwiYmdBbHBoYSI6MSwiYnJhbmRCZyI6IiIsImJyYW5kQmdBbHBoYSI6MSwiYnJhbmRUZXh0IjoiIiwiZm9udCI6InNvcmEiLCJnb29nbGVGb250IjoiIiwic2hvd0JvcmRlciI6dHJ1ZSwic2hhZG93IjpmYWxzZSwiZ2xhc3MiOmZhbHNlLCJtb3Rpb24iOnRydWV9LCJtb2RlIjoibm9ybWFsIiwicmVmcmVzaCI6MCwidGl0bGUiOiIifQ" data-height="1879">
-  <a href="https://robolyst.org/team/ftc/23975">Robolyst — Team 23975</a>
+<h1 class = {title_class_def}>Achievements</h1>
+<br>
+
+<!-- Robolyst Team Embed -->
+<div data-robolyst-embed="https://robolyst.org/embed?c=eyJ2IjoxLCJzdWJqZWN0Ijp7ImtpbmQiOiJ0ZWFtIiwiaWQiOiIyMzk3NSIsInNlYXNvbiI6bnVsbH0sImJsb2NrcyI6W3sidCI6InRlYW0tY2FyZCIsInMiOnsic2hvd0xvY2F0aW9uIjp0cnVlLCJzaG93Um9va2llIjp0cnVlLCJzaG93UmVjb3JkIjp0cnVlLCJzaG93RXZlbnRzIjp0cnVlLCJzaG93QXdhcmRzIjp0cnVlfX0seyJ0IjoidGVhbS11cGNvbWluZyIsInMiOnsiY291bnQiOjMsInNob3dDb3VudGRvd24iOnRydWV9fSx7InQiOiJ0ZWFtLWV2ZW50cyIsInMiOnsiY291bnQiOjMsInNob3dSZWNvcmQiOnRydWUsInNob3dSYW5rIjp0cnVlLCJzaG93QXdhcmRzIjp0cnVlLCJzaG93RGF0ZXMiOnRydWV9fSx7InQiOiJ0ZWFtLW1ldHJpY3MiLCJzIjp7Im1ldHJpY3MiOlsib3ByIiwib3ByYyIsInN0cmVuZ3RoIl0sImJhc2lzIjoiYmVzdCIsImxheW91dCI6ImdyaWQifX0seyJ0IjoidGVhbS1hd2FyZHMiLCJzIjp7ImxpbWl0Ijo2MCwiZ3JvdXBi yU2Vhc29uIjp0cnVlLCJzdHlsZSI6IndhbGwifX1dLCJsYXlvdXQiOnsidyI6MCwiaCI6MTg3OSwiY29scyI6MSwiZ2FwIjoxMiwicGFkIjoxNiwicmFkaXVzIjoxNCwiZGVuc2l0eSI6ImNvbWZvcnRhYmxlIiwiZGlyIjoiY29sdW1uIn0sInRoZW1lIjp7InByZXNldCI6ImRhcmsiLCJiZyI6IjAyMDQxMCIsInN1cmZhY2UiOiIxNjE2MTYiLCJib3JkZXIiOiIyNzI2MjUiLCJ0ZXh0IjoiZmR mZGYiLCJtdXRlZCI6Ijg4ODg4OCIsImFjY2VudCI6IjhjNTJmZiIsInBvcyI6IjRhZGU4MCIsIm5lZyI6ImY4NzE3MSIsImJnQWxwaGEiOjEsImJyYW5kQmciOiIiLCJicmFuZEJnQWxwaGEiOjEsImJyYW5kVGV4dCI6IiIsImZvbnQiOiJzb3JhIiwiZ29vZ2xlRm9udCI6IiIsInNob3dCb3JkZXIiOnRydWUsInNoYWRvdyI6ZmFsc2UsImdsYXNzIjpmYWxzZSwibW90aW9uIjp0cnVlfSwibW9kZSI6Im5vcm1hbCIsInJlZnJlc2giOjAsInRpdGxlIjoiIn0" data-height="1879">
+    <a href="https://robolyst.org/team/ftc/23975">Robolyst — Team 23975</a>
 </div>
+
 <script src="https://robolyst.org/embed.js" async></script>
 
 <br>
-
 
 {#if done}
     {#each data.order as yearID, i}
         <h2 class = {title_secondary_class_def}>{yearID}</h2>
         <br>
+
         {#each data[yearID] as item, j}
             <b><i><u>{item.title}</u></i></b>
             <br>
@@ -35,6 +39,7 @@
                     <li>{bullet}</li>
                 {/each}
             </ul>
+
             {#if data.order.length -1 != i && data[yearID].length -1 == j}
                 <hr>
             {:else}
@@ -42,70 +47,4 @@
             {/if}
         {/each}
     {/each}
-
 {/if}
-
-<!-- 
-<h2 class = {title_secondary_class_def}>2022 - 2023</h2>
-<br>
-<b><i><u>Union Bridge Qualifier - 3rd Qualifier (Jan. 28)</u></i></b>
-<br>
-<ul>
-    <li>Finalist Alliance Captain</li>
-    <li>Design Award 2nd Place</li>
-    <li>OPR of 96.8</li>
-</ul>
-
-<br>
-<b><i><u>McHenry Qualifier - 2nd Qualifier (Jan. 21)</u></i></b>
-<br>
-<ul>
-    <li>Winning Alliance Captain</li>
-    <li>Design Award 1st Place</li>
-    <li>Motivate Award 2nd Place</li>
-    <li>OPR of 106.6</li>
-</ul>
-
-<br>
-<b><i><u>Mechanicsville Qualifier - 1st Qualifier (Dec. 3)</u></i></b>
-<br>
-<ul>
-    <li>Winning Alliance Captain</li>
-    <li>Design Award 1st Place</li>
-    <li>OPR of 115.8</li>
-</ul>
-<br>
-<hr>
-<br>
-<h2 class = {title_secondary_class_def}>2021 - 2022</h2>
-<br>
-<b><i><u>Alexandria Qualifier 4 – 3rd Qualifier (Jan. 22)</u></i></b>
-<br>
-<ul>
-    <li>Ranked 1st Place</li>
-    <li>818 Ranking Points</li>
-    <li>215 High Score</li>
-    <li>4 – 1 – 0 Record</li>
-</ul>
-
-<br>
-<b><i><u>Alexandria Qualifier 1 – 2nd Qualifier (Jan. 15)</u></i></b>
-<br>
-<ul>
-    <li>Ranked 12th Place</li>
-    <li>526 Ranking Points</li>
-    <li>189 High Score</li>
-    <li>3 – 2 – 0 Record</li>
-    
-</ul>
-
-<br>
-<b><i><u>Columbia Qualifier 2 – 1st Qualifier (Dec. 4)</u></i></b>
-<br>
-<ul>
-    <li>Ranked 4th Place</li>
-    <li>366 Ranking Points</li>
-    <li>101 High Score</li>
-    <li>5 – 0 – 0 Record</li>
-</ul> -->
-
